4aa8b41887c49dc238a7949031300e153f18f8c4
omar-faruk-bdtask <omar@bdtask.net>
2026-04-13 18:47:24 +0600

    API issue solved

--- apps/web/lib/api.js
+++ apps/web/lib/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-// need dotenv
-import dotenv from "dotenv";
-dotenv.config();
+
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api",
